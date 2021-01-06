d55a15eac21a52981b4bc84d4f5bc3052e7bb165
Dan6erbond <moravrav@gmail.com>
2021-01-06 23:46:24 +0100

    Add make search by ID and name, update docstrings

--- server/src/graphql/schema/make.js
+++ server/src/graphql/schema/make.js
@@ -11,6 +11,7 @@ import Knex from "knex";
 export const typeDef = gql`
   extend type Query {
     makes: [Make!]!
+    make(id: ID, name: String): Make
   }
 
   type Make {
@@ -23,7 +24,7 @@ export const typeDef = gql`
 export const resolvers = {
   Query: {
     /**
-     *
+     * Fetch all the makes in the database.
      * @param {Object} _
      * @param {Object} __
      * @param {Object} ctx GraphQL context variables.
@@ -37,10 +38,34 @@ export const resolvers = {
       const makes = await db.select("*").from("makes");
       return makes;
     },
+    /**
+     * Fetch a make by ID or name.
+     * @param {Object} _
+     * @param {Object} args GraphQL query arguments.
+     * @param {string} args.id The ID to retrieve a make by.
+     * @param {string} args.name The name to retrieve a make by.
+     * @param {Object} ctx GraphQL context variables.
+     * @param {Knex} ctx.db The Knex DB instance.
+     * @returns {?Make}
+     */
+    make: async (_, { id, name }, { db }) => {
+      let where = {};
+      if (id) {
+        where = { ...where, id };
+      }
+      if (name) {
+        where = { ...where, name };
+      }
+      /**
+       * @type {(?Make)}
+       */
+      const make = await db.first("*").from("makes").where(where);
+      return make;
+    },
   },
   Make: {
     /**
-     *
+     * Fetch all the cars built by the make.
      * @param {Make} parent The parent make object.
      * @param {Object} _
      * @param {Object} ctx GraphQL context variables.
@@ -48,6 +73,9 @@ export const resolvers = {
      * @returns {import("./car").Car[]}
      */
     cars: async (parent, _, { db }) => {
+      /**
+       * @type {import("./car").Car[]}
+       */
       const cars = await db.select("*").from("cars").where({ make_id: parent.id });
       return cars;
     },
