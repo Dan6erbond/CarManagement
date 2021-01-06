139ac5395f5bf5132a7e5d78d3cddbb3fdb5dc5f
Dan6erbond <moravrav@gmail.com>
2021-01-06 19:40:49 +0100

    Bootstrap NodeJS backend with GraphQL, Knex, Express and Babel/ESLint

---
+++ server/src/knex/knexfile.js
@@ -0,0 +1,11 @@
+// Update with your config settings.
+var path = require("path");
+
+module.exports = {
+  development: {
+    client: "sqlite3",
+    connection: {
+      filename: path.join(__dirname, "..", "..", "tmp", "db.sqlite"),
+    },
+    useNullAsDefault: true,
+  },
