77b950f6ce7978eb7a278526e16e47eeb4142ef9
Dan6erbond <moravrav@gmail.com>
2021-01-07 13:27:54 +0100

    Create Knex migration stub and add typehints

--- server/src/knex/knexfile.js
+++ server/src/knex/knexfile.js
@@ -1,6 +1,10 @@
 // Update with your config settings.
+const Knex = require("knex");
 var path = require("path");
 
+/**
+ * @type {Knex.Config<any>}
+ */
 module.exports = {
   development: {
     client: "sqlite3",
@@ -8,5 +12,8 @@ module.exports = {
       filename: path.join(__dirname, "..", "..", "tmp", "db.sqlite"),
     },
     useNullAsDefault: true,
+    migrations: {
+      stub: "migration.stub",
+    },
   },
-}
+};
