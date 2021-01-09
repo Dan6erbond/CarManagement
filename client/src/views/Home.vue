6b3175b6460035b5960134532666328c813b852a
Dan6erbond <moravrav@gmail.com>
2021-01-09 11:29:58 +0100

    Update home component to card with "View all cars" only, update styling

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,13 +1,17 @@
 <template>
-  <div class="home">
-    <div class="columns">
-      <div class="column">
+  <div class="home pt-2 px-6">
+    <div class="columns is-multiline">
+      <div class="column is-one-quarter-desktop">
         <div class="card">
           <div class="card-content">
             <div class="content">
               <p class="is-size-2">Cars</p>
-              <p><router-link to="/cars">View all the cars</router-link></p>
-              <p><router-link to="/makes">By make</router-link></p>
+              <p>
+                <router-link to="/cars">
+                  View all the cars
+                  <b-icon icon="arrow-right" size="is-small"></b-icon>
+                </router-link>
+              </p>
             </div>
           </div>
         </div>
@@ -17,7 +21,12 @@
           <div class="card-content">
             <div class="content">
               <p class="is-size-2">Customers</p>
-              <p><router-link to="/makes">Manage customers</router-link></p>
+              <p>
+                <router-link to="/customers">
+                  Manage customers
+                  <b-icon icon="arrow-right" size="is-small"></b-icon>
+                </router-link>
+              </p>
             </div>
           </div>
         </div>
@@ -27,7 +36,12 @@
           <div class="card-content">
             <div class="content">
               <p class="is-size-2">Rentals</p>
-              <p><router-link to="/makes">Manage rentals</router-link></p>
+              <p>
+                <router-link to="/rentals">
+                  Manage rentals
+                  <b-icon icon="arrow-right" size="is-small"></b-icon>
+                </router-link>
+              </p>
             </div>
           </div>
         </div>
@@ -41,3 +55,10 @@ export default {
   name: "Home",
 };
 </script>
+
+
+<style lang="scss" scoped>
+.home {
+  flex-grow: 1;
+}
+</style>
