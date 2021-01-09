e720464b68df65134346bb16d189994cddf48e14
Dan6erbond <moravrav@gmail.com>
2021-01-09 10:00:56 +0100

    Setup basic Buefy navbar in App, remove Home image

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,9 +1,29 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <b-navbar>
+      <template slot="brand">
+        <b-navbar-item tag="router-link" :to="{ path: '/' }">
+          <img
+            src="https://raw.githubusercontent.com/buefy/buefy/dev/static/img/buefy-logo.png"
+            alt="Lightweight UI components for Vue.js based on Bulma"
+          />
+        </b-navbar-item>
+      </template>
+      <template slot="start">
+        <b-navbar-item tag="router-link" to="/rentals">Rentals </b-navbar-item>
+        <b-navbar-dropdown label="Cars">
+          <b-navbar-item tag="router-link" to="/makes">By Make</b-navbar-item>
+          <b-navbar-item tag="router-link" to="/cars">All</b-navbar-item>
+        </b-navbar-dropdown>
+        <b-navbar-item tag="router-link" to="/customers">Customers</b-navbar-item>
+      </template>
+
+      <template slot="end">
+        <b-navbar-item tag="div">
+          © 2021, RaviAnand M.
+        </b-navbar-item>
+      </template>
+    </b-navbar>
     <router-view />
   </div>
 </template>
