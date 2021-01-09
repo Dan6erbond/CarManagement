3f6205c4bb85353d763b2d335ba3883bc63f518e
Dan6erbond <moravrav@gmail.com>
2021-01-09 09:55:51 +0100

    Setup Prettier styling rules and remove boilerplate

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,14 +1,27 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
   },
   extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
   parserOptions: {
-    parser: "babel-eslint"
+    parser: "babel-eslint",
   },
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
-  }
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "prettier/prettier": [
+      "warn",
+      {
+        printWidth: 120,
+        tabWidth: 2,
+        tabs: false,
+        semi: true,
+        singleQuote: false,
+        trailingComma: "all",
+        bracketSpacing: true,
+        jsxBracketSameLine: false,
+      },
+    ],
+  },
 };
